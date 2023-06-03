d592e1854da3170b094c6d33dae18ff2f34ea635
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-06-03 20:50:14 +0300

    Create 733. Flood Fill leetcode DSa.py

---
+++ 733. Flood Fill leetcode DSa.py
@@ -0,0 +1,26 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        if image[sr][sc] == newColor:
+            return image
+        
+        visited = set()
+        queue = [(sr, sc)]
+        color = image[sr][sc]
+        while queue:
+            r, c = queue.pop(0)
+            if (r, c) in visited or r < 0 or r >= len(image) or c < 0 or c >= len(image[0]) or image[r][c] != color:
+                continue
+            
+            visited.add((r, c))
+            queue.append((r-1, c)) # top neighbor
+            queue.append((r+1, c)) # bottom neighbor
+            queue.append((r, c-1)) # left neighbor
+            queue.append((r, c+1)) # right neighbor
+        
+        for r, c in visited:
+            image[r][c]= newColor
+        
+        return image
+
+
+# https://leetcode.com/problems/flood-fill/description/
